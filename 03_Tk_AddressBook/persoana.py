44857f1165a4a7d29f8fdeb34c5e3b6c92997637
mhcrnl <mhcrnl@gmail.com>
2021-10-11 18:20:52 +0300

    luni 11 octombrie 2021, 18:20:49 +0300

---
+++ 03_Tk_AddressBook/persoana.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+# coding: utf-8
+# author: mhcrnl@gmail.com
+# file  : persoana.py
+#-----------------------------------------------------------------
+class Persoana:
+    """ Clasa persoana. """
+    
+    def __init__(self, nume, prenume, telefon, email, onomastica):
+        self.nume = nume
+        self.prenume = prenume
+        self.telefon = telefon
+        self.email = email
+        self.onomastica = onomastica
